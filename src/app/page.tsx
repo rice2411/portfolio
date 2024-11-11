330e575660a79f917b0bf56c62d801405319c485
PhucLH <phuclh@rikkeisoft.com>
2024-11-11 14:49:47 +0700

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+import { Contact } from "@/components/Contact";
+import { Footer } from "@/components/Footer";
+import { Hero } from "@/components/Hero";
+import { KeyMetrics } from "@/components/KeyMetrics";
+import { LogoAnimation } from "@/components/LogoAnimation";
+import { Navbar } from "@/components/Navbar";
+import { Portfolio } from "@/components/Portfolio";
+import { Services } from "@/components/Services";
+import { Stack } from "@/components/Stack";
+
+export default function Home() {
+  return (
+    <>
+        <Navbar/>
+        <Hero/>
+        <Stack/>
+        <LogoAnimation/>
+        <Portfolio/>
+        <KeyMetrics/>
+        <Services/>
+        <Contact/>
+        <Footer/>
+    </>  
+    );
+}
